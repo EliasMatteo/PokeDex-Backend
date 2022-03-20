70b371dc873b89cdce559e4a1176ffab01249fc0
EliasMatteo <eflamencosolito@my.bcit.ca>
2022-03-20 05:24:54 -0700

    auth routes for users

--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// const { signUp, signIn } = require("../controller/controller");
-
-// router.get("/signUp", signUp);
-// router.post("/signIn", signIn);
-
-module.exports = router;
