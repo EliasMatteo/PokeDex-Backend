b5dc4fd8872f61cde26b217c3d0b860735fbee31
EliasMatteo <eflamencosolito@my.bcit.ca>
2022-03-20 05:25:50 -0700

    app.js setup for rest of user setup and app itself

--- app.js
+++ app.js
@@ -1,46 +1,37 @@
-//Base important imports
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-
-//File imports
-const config = require("./config");
-const appRouter = require("./routes/routes");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+//import routes
+const authRoutes = require("./routes/authRoutes");
+const { db } = require("./models/User");
+//app
+const app = express();
 
 //Port specfication
 // const port = 5000;
 
+//middlewares
+app.use(bodyParser.json());
+app.use(cors());
+//routes middleware
+app.use(authRoutes);
 /*--------------------*/
 //App code
 
-app.use(cors());
-app.use(appRouter);
-app.use(express.json());
-
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ phrase: "hello PokeDex Test!" });
-  res.status(503).send(console.log("why isnt this working"));
-});
-
-// app.post("/pokePost", (req, res) => {
-//   let body = req.body;
-//   res.send(req.body.value);
-// });
-
 // DB connections and ports
-app.listen(process.env.PORT || 5000, () => {
-  console.log("work please");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
 });
 
-mongoose.connect(config.MONGODB_URL, () => {
-  console.log("MongoDB connected");
-});
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connected"));
